52c7523cf578ff7d333b27b0aaea3fba66ce6438
umenokoudai <umenokoudai0805@gmail.com>
2023-04-29 19:23:39 +0900

    初コミット

---
+++ Assets/ItemData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class ItemData
+{
+    public int _category;
+    public int _itemNo;
+    public int _itemCount;
+}
