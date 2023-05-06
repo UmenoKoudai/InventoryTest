1976299307b203a1be705ec6a3a71e201edb071b
umenokoudai <umenokoudai0805@gmail.com>
2023-05-06 23:46:08 +0900

    またうまく動かない結構シンプルにしたのに

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] GameObject _inventory;
+    [SerializeField] Sprite _axe;
+    int[] _itemArray;
+    int _itemBoxCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _itemBoxCount = _inventory.transform.childCount;
+        _itemArray = Enumerable.Repeat(-1, _itemBoxCount).ToArray();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetItem(int item)
     {
-        
+        for(int i = 0; i < _itemBoxCount; i++)
+        {
+            if (_itemArray[i] == -1)
+            {
+                _itemArray[i] = item;
+            }
+            break;
+        }
+    }
+    public void GetItem()
+    {
+        for(int i = 0; i < _itemBoxCount; i++)
+        {
+            if (_itemArray[i] != -1)
+            {
+                var item = _inventory.transform.GetChild(i).gameObject.GetComponent<Image>();
+                switch(_itemArray[i])
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        item.sprite = _axe;
+                        //item = (GameObject)Resources.Load("axe");
+                        break;
+                }
+                break;
+            }
+        }
     }
 }
