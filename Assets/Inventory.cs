6c02e02c01e179d3fc985e39246939faaa802174
umenokoudai <umenokoudai0805@gmail.com>
2023-05-14 22:48:38 +0900

    アイテムボタンの背景を設定した。 今はアイテム配列を保存するようにしている

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -4,10 +4,13 @@ using UnityEngine.UI;
 
 public class Inventory : InstanceSystem<Inventory>
 {
+    public class ItemData { public ItemState[] _itemArray; }
+
     [SerializeField] GameObject _inventory;
     [SerializeField] Sprite _defaultImage;
     ItemState[] _itemArray;
     int _itemBoxCount;
+    ItemData _itemData = new ItemData();
 
     public ItemState[] ItemArray { get => _itemArray; }
     public Sprite DefaultImage { get => _defaultImage; }
@@ -17,6 +20,13 @@ public class Inventory : InstanceSystem<Inventory>
     {
         _itemBoxCount = _inventory.transform.childCount;
         _itemArray = Enumerable.Repeat(new ItemState("なし", -1, 0, _defaultImage), _itemBoxCount).ToArray();
+        //_itemData._itemArray = _itemArray;
+        //_itemData = _itemData.OnLoad();
+        //if (_itemData != null)
+        //{
+        //    _itemArray = _itemData._itemArray;
+        //}
+        SetItem();
     }
     public void Debuger()
     {
@@ -29,7 +39,8 @@ public class Inventory : InstanceSystem<Inventory>
     {
         for (int i = 0; i < _itemBoxCount; i++)
         {
-            var viewItem = _inventory.transform.GetChild(i).GetComponent<ItemButton>();
+            var viewItemParent = _inventory.transform.GetChild(i);
+            var viewItem = viewItemParent.transform.GetChild(0).GetComponent<ItemButton>();
             if (_itemArray[i]._itemID != -1)
             {
                 viewItem.ItemID = _itemArray[i]._itemID;
@@ -37,5 +48,7 @@ public class Inventory : InstanceSystem<Inventory>
                 viewItem.ItemCount = _itemArray[i]._itemCount;
             }
         }
+        //_itemData._itemArray = _itemArray;
+        //_itemData.OnSave();
     }
 }
